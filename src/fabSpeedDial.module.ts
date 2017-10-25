1e31c392a9bfd0de3e479ae45b66f15e64cb2d72
Rahul Kumar <rahul@funzamr.com>
2017-10-25 23:49:07 +0530

    code refactoring

--- src/fabSpeedDial.module.ts
+++ src/fabSpeedDial.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from "@angular/core";
-import {FlexLayoutModule} from '@angular/flex-layout';
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { FabToggleDirective, FabButtonDirective, FabSpeedDialComponent } from "./fabSpeedDial.component";
-import { MaterialCommonModule } from "../../commonMaterialModules/materialCommon.module";
 
 @NgModule({
   imports         : [
-                      MaterialCommonModule,
-                      FlexLayoutModule,
-                      CommonModule,
-                      FormsModule
+                      CommonModule
                     ],
   declarations    : [ 
                       FabToggleDirective,
